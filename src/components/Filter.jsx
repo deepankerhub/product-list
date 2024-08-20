baf4464eba7a771124c4477163cc2944d62f3f80
deepankerhub <deepanker8527@gmail.com>
2024-08-20 22:55:58 +0530

    Productlist

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 
 const Filter = ({ filters, setFilters }) => {
+  // Updated: Function to handle input changes
   const handleFilterChange = (e) => {
+    const { name, value } = e.target; // Destructured name and value from e.target
     setFilters({
-      ...filters,
-      [e.target.name]: e.target.value,
+      ...filters, // Spread operator to retain existing filters
+      [name]: value, // Updated filter based on the input's name attribute
     });
   };
 
   return (
     <div>
       <h3>Filters</h3>
-     <label htmlFor="color"> 
-        Color:</label>
+      <div>
+        {/* Updated: Added htmlFor to associate label with input */}
+        <label htmlFor="color">Color:</label> 
+        {/* Updated: Added id to input to match the label's htmlFor */}
         <input
+          id="color"
           type="text"
-          name="color"
-          value={filters.color}
-          onChange={handleFilterChange}
+          name="color" // name attribute matches the key in the filters state
+          value={filters.color} // Controlled input, value comes from the filters state
+          onChange={handleFilterChange} // Calls handleFilterChange on input change
         />
-      
+      </div>
       <br />
-      <label>
-        Capacity:
+      <div>
+        {/* Updated: Added htmlFor to associate label with input */}
+        <label htmlFor="capacity">Capacity:</label> 
+        {/* Updated: Added id to input to match the label's htmlFor */}
         <input
+          id="capacity"
           type="text"
-          name="capacity"
-          value={filters.capacity}
-          onChange={handleFilterChange}
+          name="capacity" // name attribute matches the key in the filters state
+          value={filters.capacity} // Controlled input, value comes from the filters state
+          onChange={handleFilterChange} // Calls handleFilterChange on input change
         />
-      </label>
+      </div>
     </div>
   );
 };
